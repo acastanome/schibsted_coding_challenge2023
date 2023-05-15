975e78478b87d2882855acda4c80730488885767
Roope Vuorenlehto <rvuorenl@home.fi>
2023-05-15 23:23:41 +0300

    add AverageScore.js. Calculates the average and puts emphasis on the most recent ratings

--- rating-system-app/src/App.js
+++ rating-system-app/src/App.js
@@ -1,10 +1,12 @@
 import RatingInterface from './components/RatingInterface';
+import AverageScore from './components/AverageScore';
 
 const App = () => {
 	return (
 		<div>
 			<div>Mock chats page layout</div>
 			<RatingInterface />
+			<AverageScore />
 		</div>
 	);
 };
